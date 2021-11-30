cadd264ff7906ced0f32272b99b5d5e088196541
Lucas Flóro <lucas@fearp.usp.br>
2021-11-30 16:20:00 -0300

    Remoção prefixo 'arvores' das rotas.

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ArvoreController::class, 'index'])->name('arvores.index');
-
-Route::prefix('arvores')->group(function () {
-    Route::get('/', [ArvoreController::class, 'index'])->name('arvores.index');
-    Route::get('/create', [ArvoreController::class, 'create'])->name('arvores.create')->middleware('can:admin');
-    Route::post('/', [ArvoreController::class, 'store'])->name('arvores.store')->middleware('can:admin');
-    Route::get('/show/{arvore}', [ArvoreController::class, 'show'])->name('arvores.show');
-    Route::get('/edit/{arvore}', [ArvoreController::class, 'edit'])->name('arvores.edit');
-    Route::patch('/{arvore}', [ArvoreController::class, 'update'])->name('arvores.update')->middleware('can:admin');
-    Route::delete('/{arvore}', [ArvoreController::class, 'destroy'])->name('arvores.destroy')->middleware('can:admin');
-    Route::get('/especies/create', [EspecieController::class, 'create'])->name('especies.create')->middleware('can:admin');
-    Route::get('/especies/', [EspecieController::class, 'index'])->name('especies.index')->middleware('can:admin');
-    Route::get('/especies/edit/{especie}', [EspecieController::class, 'edit'])->name('especies.edit')->middleware('can:admin');
-    Route::patch('/especies/{especie}', [EspecieController::class, 'update'])->name('especies.update')->middleware('can:admin');
-    Route::get('/ocorrencias/create/{arvore}', [OcorrenciaController::class, 'create'])->name('ocorrencias.create')->middleware('can:admin');
-    Route::post('/ocorrencias/', [OcorrenciaController::class, 'store'])->name('ocorrencias.store')->middleware('can:admin');
-    Route::get('/ocorrencias/{ocorrencia}', [OcorrenciaController::class, 'edit'])->name('ocorrencias.edit')->middleware('can:admin');
-    Route::patch('/ocorrencias/{ocorrencia}', [OcorrenciaController::class, 'update'])->name('ocorrencias.update')->middleware('can:admin');
-    Route::post('/especies/', [EspecieController::class, 'store'])->name('especies.store')->middleware('can:admin');
-    Route::resource('/arquivos', ArquivoController::class);
-});
+Route::get('/create', [ArvoreController::class, 'create'])->name('arvores.create')->middleware('can:admin');
+Route::post('/', [ArvoreController::class, 'store'])->name('arvores.store')->middleware('can:admin');
+Route::get('/show/{arvore}', [ArvoreController::class, 'show'])->name('arvores.show');
+Route::get('/edit/{arvore}', [ArvoreController::class, 'edit'])->name('arvores.edit');
+Route::patch('/{arvore}', [ArvoreController::class, 'update'])->name('arvores.update')->middleware('can:admin');
+Route::delete('/{arvore}', [ArvoreController::class, 'destroy'])->name('arvores.destroy')->middleware('can:admin');
+Route::get('/especies/create', [EspecieController::class, 'create'])->name('especies.create')->middleware('can:admin');
+Route::get('/especies/', [EspecieController::class, 'index'])->name('especies.index')->middleware('can:admin');
+Route::get('/especies/edit/{especie}', [EspecieController::class, 'edit'])->name('especies.edit')->middleware('can:admin');
+Route::patch('/especies/{especie}', [EspecieController::class, 'update'])->name('especies.update')->middleware('can:admin');
+Route::get('/ocorrencias/create/{arvore}', [OcorrenciaController::class, 'create'])->name('ocorrencias.create')->middleware('can:admin');
+Route::post('/ocorrencias/', [OcorrenciaController::class, 'store'])->name('ocorrencias.store')->middleware('can:admin');
+Route::get('/ocorrencias/{ocorrencia}', [OcorrenciaController::class, 'edit'])->name('ocorrencias.edit')->middleware('can:admin');
+Route::patch('/ocorrencias/{ocorrencia}', [OcorrenciaController::class, 'update'])->name('ocorrencias.update')->middleware('can:admin');
+Route::post('/especies/', [EspecieController::class, 'store'])->name('especies.store')->middleware('can:admin');
+Route::resource('/arquivos', ArquivoController::class);
 
 Route::resource('foto', FotoController::class);

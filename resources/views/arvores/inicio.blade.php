b4aefbc32c7a1f5eb00a4b4fcb9f43186972b586
Lucas Flóro <lucas@fearp.usp.br>
2022-03-21 16:12:03 -0300

    Alterações textuais na página inicial.

--- resources/views/arvores/inicio.blade.php
+++ resources/views/arvores/inicio.blade.php
@@ -4,8 +4,11 @@
         <div class="jumbotron">
             <div class="container">
                 <h1 class="display-4"><span class="align-middle"><img src="{{ asset('logo_fearp_arvore_transparente.png') }}"> ÁRVORES da fea-RP!</span></h1>
+                <hr>
                 <p>O site Árvores da FEA-RP é um sistema on-line criado em 2022, em trabalho conjunto das áreas administrativa, comunicação e de informática da FEA-RP, com apoio do Sr. Antônio Justino, chefe do Serviço de Áreas Verdes e Meio Ambiente da Prefeitura do Campus. O sistema tem o objetivo de apresentar à comunidade as árvores da Faculdade bem como acompanhar seu manejo (ocorrência de podas, análise de risco etc.).</p>
                 <p>Além das cerca de 250 árvores catalogadas no momento da criação do sistema, a Faculdade possui outro conjunto arbóreo não contabilizado, localizado em uma área preservada delimitada pelo Bloco B1, Rua Pedreira de Freitas, Bloco C1 e Cantina.</p>
+                <p>Grupos de árvores de grande porte receberam placas de identificação e um QR Code específico. O usuário poderá escanear o código com seu celular e será direcionado à página da árvore em questão e visualizar as informações dela.</p>
+                <p>No sistema, o usuário poderá acessar diversas informações das plantas da faculdade e interagir, comentando e compartilhando nas redes sociais Facebook, Twitter e LinkedIn. Entre as informações disponíveis estão os nomes popular e científico, porte, localização, foto e histórico de ocorrências.</p>
             </div>
         </div>
         <div class="accordion" id="accordionExample">
@@ -13,14 +16,17 @@
             <div class="card-header" id="headingTwo">
               <h2 class="mb-0">
                 <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    QUAIS INFORMAÇÕES ESTÃO DISPONÍVEIS? <span class="text-right"><i class="fas fa-plus"></i></span>
+                    COMO UTILIZAR? <span class="text-right"><i class="fas fa-plus"></i></span>
                 </button>
               </h2>
             </div>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
               <div class="card-body">
-                <p>Todas as árvores receberam placas de identificação e um QR Code específico. O usuário poderá escanear o código com seu celular e será direcionado à página da árvore em questão e visualizar as informações dela.</p>
-                <p>No sistema, o usuário poderá acessar diversas informações das plantas da faculdade e interagir, comentando e compartilhando nas redes sociais Facebook, Twitter e LinkedIn. Entre as informações disponíveis estão os nomes popular e científico, porte, localização, foto e histórico de ocorrências.</p>
+                  A navegação é feita pelos itens da barra superior: <br><br>
+                <ul>
+                    <li><a href="{{ route('arvores.index') }}"><i>Listagem completa</a></i> exibe todas as árvores catalogadas. A partir dessa lista é possível acessar a página de cada árvore</li>
+                    <li><i><a href="{{ route('arvores.mapa') }}">Mapa com todas árvores</a></i> exibe um mapa do google com as marcações de localização das árvores</li>
+                </ul>
               </div>
             </div>
           </div>
@@ -28,17 +34,13 @@
             <div class="card-header" id="headingThree">
               <h2 class="mb-0">
                 <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    COMO UTILIZAR? <span class="text-right"><i class="fas fa-plus"></i></span>
+                    FLUXOGRAMA DE PODA <span class="text-right"><i class="fas fa-plus"></i></span>
                 </button>
               </h2>
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
               <div class="card-body">
-                  A navegação é feita pelos itens da barra superior: <br><br>
-                <ul>
-                    <li><a href="{{ route('arvores.index') }}"><i>Listagem completa</a></i> exibe todas as árvores catalogadas. A partir dessa lista é possível acessar a página de cada árvore</li>
-                    <li><i><a href="{{ route('arvores.mapa') }}">Mapa com todas árvores</a></i> exibe um mapa do google com as marcações de localização das árvores</li>
-                </ul>
+                  <img src="{{ asset('fluxograma_poda.png') }}">
               </div>
             </div>
           </div>
@@ -64,6 +66,7 @@
                     Leonardo Rezende (Assistente de Comunicação)
               </div>
             </div>
+          </div>
           <div class="card">
             <div class="card-header" id="headingFive">
               <h2 class="mb-0">
